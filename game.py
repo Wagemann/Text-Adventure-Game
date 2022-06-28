26c7bfcb4685308bf59c6a66a6fd81a1c86f19f2
Nick <wagemannnick@gmail.com>
2022-06-27 20:51:32 -0400

    fixed spaceship run

--- game.py
+++ game.py
@@ -85,12 +85,86 @@ if setting == 'The Abandoned Building':
         print(f'invalid response! You lose and your team was never seen again!')
         quit()
 
-elif setting == "The Distressed Starship":
+elif setting == "The Distressed Spaceship":
     print(f"Are you sure {name} you would'nt rather go to The Abandoned Building?")
     response = input("Yes or No ")
     if response == "Yes":
         print("Great best of luck!")
-        quit()
+        print(f"Welcome {name} to the former headquarters to the parasol corporation. You were told to remain lookout while the other members of the team began exploring the building.")
+        response = input("It has been awhile since team radioed in...do you \'stay\' at the front of the building or do you \'enter\'? ")
+        if response == "stay":
+            print("You wait another 30 minutes and you here nothing over the radio.")
+            response = input("Do you \'wait\' longer or do you \'enter\'? ")
+            if response == 'wait':
+                print("You wait another 30 minutes then a cop comes up and tells you to put your hands up in the air.")
+                response = input("Do you put your \'hands up\' or do you run and \'enter\' the building? ")
+                if response == "hands up":
+                    print("You are arrested for trespassing. Later convicted of murder of your team members as they were all found dead in the building days later.")
+                    quit()
+                elif response == 'enter':
+                    print("You enter the abandonded building. As you enter you see shattered glass and rubble all over the floors, the walls covered in graffiti and a you see a open door leading to a staircase to the next level.")
+                    response = input("Do you wait in the \'foyer\' or do you go up the \'stairs\'? ")
+                else:
+                    print('invalid respone! You lose and your team was never seen again!')
+                    if response == 'stairs':
+                        print('You climb the staircase and see a madman with a bat about to beat your friends')
+                        response = input("Do you \'attack\' the madman from behind or \'do nothing\'? ")
+                        if response == 'attack':
+                            print('You knockout the madman as he is about to kill one of your teammates just as you do a policeman arrives on scene and handcuffs the madman and lets you all of with a warning for tresspassing. Your friends are so grateful for you saving them that they buy you ice cream.')
+                            quit()
+                        elif response == 'do nothing':
+                            print("You watch as all of team members are beaten to death and then a police officer arrives and arrests you and the madman for the murder.")
+                            quit()
+                        else:
+                            print('invalid respone! You lose and your team was never seen again!')
+                    elif response == 'foyer':
+                        print("You are arrested for trespassing. Later convicted of murder of the team members as they were all found dead in the building days later.")
+                        quit()
+                    else:
+                        print('invalid respone! You lose and your team was never seen again!')
+            elif response == 'enter':
+                print("You enter the abandonded building. As you enter you see shattered glass and rubble all over the floors, the walls covered in graffiti and a you see a open door leading to a staircase to the next level.")
+                response = input("Do you wait in the \'foyer\' or do you go up the \'stairs\'? ")
+                if response == 'stairs':
+                    print('You climb the staircase and see a madman with a bat about to beat your friends')
+                    response = input("Do you \'attack\' the madman from behind or \'do nothing\'? ")
+                    if response == 'attack':
+                        print('You knockout the madman as he is about to kill one of your teammates just as you do a policeman arrives on scene and handcuffs the madman and lets you all of with a warning for tresspassing. Your friends are so grateful for you saving them that they buy you ice cream.')
+                        quit()
+                    elif response == 'do nothing':
+                        print("You watch as all of team members are beaten to death and then a police officer arrives and arrests you and the madman for the murder.")
+                        quit()
+                    else:
+                        print('invalid response! You lose and your team was never seen again!')
+                elif response == 'foyer':
+                    print(f"You are arrested for trespassing. Later convicted of murder of your team members as they were all found dead in the building days later.")
+                    quit()
+                else:
+                    print(f'invalid response! You lose and your team was never seen again!')
+            else:
+                print(f'invalid response! You lose and your team was never seen again!')    
+        elif response == "enter":
+            print("You enter the abandonded building. As you enter you see shattered glass and rubble all over the floors, the walls covered in graffiti and a you see a open door leading to a staircase to the next level.")
+            response = input("Do you wait in the \'foyer\' or do you go up the \'stairs\'? ")
+            if response == 'stairs':
+                print('You climb the staircase and see a madman with a bat about to beat your friends')
+                response = input("Do you \'attack\' the madman from behind or \'do nothing\'? ")
+                if response == 'attack':
+                    print('You knockout the madman as he is about to kill one of your teammates just as you do a policeman arrives on scene and handcuffs the madman and lets you all of with a warning for tresspassing. Your friends are so grateful for you saving them that they buy you ice cream.')
+                    quit()
+                elif response == 'do nothing':
+                    print("You watch as all your team members are beaten to death and then a police officer arrives and arrests you and the madman for the murder.")
+                    quit()
+                else:
+                    print('invalid response! You lose and your team was never seen again!')
+            elif response == 'foyer':
+                print(f"You are arrested for trespassing. Later convicted of murder of your team members as they were all found dead in the building days later.")
+                quit()
+            else:
+                print(f'invalid response! You lose and your team was never seen again!')    
+        else:
+            print(f'invalid response! You lose and your team was never seen again!')
+            quit()
     elif response == "No":
         print("Too bad cause no ticket to space for you and your team!")
         quit()
